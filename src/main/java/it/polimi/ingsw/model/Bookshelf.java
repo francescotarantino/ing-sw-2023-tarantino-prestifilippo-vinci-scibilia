37147b1086e586cfd60b29f40a3fe457b0ea610a
Francesco Tarantino <5649257+francescotarantino@users.noreply.github.com>
2023-03-14 16:23:47 +0100

    First implementation of Bookshelf

---
+++ src/main/java/it/polimi/ingsw/model/Bookshelf.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw.model;
+
+public class Bookshelf {
+    private final String playerName;
+
+    private Tile[][] matrix;
+
+    public Bookshelf(String playerName) {
+        this.playerName = playerName;
+
+
+        this.matrix = new Tile[5][6];
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                this.matrix[i][j] = null; // Forse è inutile
+            }
+        }
+    }
+
+    public String getPlayerName() {
+        return this.playerName;
+    }
+
+    public Tile getTile(int x, int y) {
+        if(x < 0 || x >= 5 || y < 0 || y >= 6)
+            throw new IndexOutOfBoundsException();
+
+        return this.matrix[x][y];
+    }
+
+    public void insertTile(int x, int y, Tile tile) {
+        this.matrix[x][y] = tile;
+    }
+}
