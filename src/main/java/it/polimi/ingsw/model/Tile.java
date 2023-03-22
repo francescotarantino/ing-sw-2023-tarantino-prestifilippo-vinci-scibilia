a29f7ec34e058d871f1994eaf7ec31223c081118
Francesco Tarantino <5649257+francescotarantino@users.noreply.github.com>
2023-03-22 09:06:14 +0100

    @Override of equals in Tile class

--- src/main/java/it/polimi/ingsw/model/Tile.java
+++ src/main/java/it/polimi/ingsw/model/Tile.java
@@ -3,8 +3,8 @@ package it.polimi.ingsw.model;
 import it.polimi.ingsw.Constants;
 
 public class Tile {
-    private Constants.TileType type;
-    private int variant;
+    private final Constants.TileType type;
+    private final int variant;
 
     // Default variant is 0 when not specified (useful for placeholders)
     public Tile (Constants.TileType type){
@@ -19,14 +19,33 @@ public class Tile {
         this.type = type;
         this.variant = variant;
     }
+
     public Tile (Tile oldTile){
         this.type = oldTile.getType();
         this.variant = oldTile.getVariant();
     }
+
     public Constants.TileType getType(){
         return this.type;
     }
+
     public int getVariant() {
         return this.variant;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Tile tile)) {
+            return false;
+        }
+        return tile.getType() == this.type && tile.getVariant() == this.variant;
+    }
+
+    @Override
+    public String toString() {
+        return "Tile: " + type +"." + variant;
     }
 }
