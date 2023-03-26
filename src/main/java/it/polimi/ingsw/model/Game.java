8613595296325ff583b5b475ee5297a529c02fe1
Francesco Tarantino <5649257+francescotarantino@users.noreply.github.com>
2023-03-26 17:58:03 +0200

    addBookshelf method improved

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -26,23 +26,25 @@ public class Game {
         randomNumbers = extractRandomCardIDs(numberOfPlayers);
         //generating an adequate amount of bookshelves and creating the first bookshelf for the first player
         this.bookshelves = new Bookshelf[numberOfPlayers];
-        bookshelves[0] = new Bookshelf(newPlayer,new PersonalGoalCard(randomNumbers[0]));
+        this.addBookshelf(newPlayer);
     }
-    public void addShelf(Player player){
-        int found = -1;
-        for (int i = 0; i < this.getTotalPlayersNumber() && found==-1; i++) {
+
+    public void addBookshelf(Player player){
+        int i;
+        for (i = 0; i < this.getTotalPlayersNumber(); i++) {
             if (this.bookshelves[i] != null){
-                found = i;
                 if(this.bookshelves[i].getPlayer().getUsername().equals(player.getUsername())) {
                     throw new IllegalArgumentException("Username already present in this game");
                 }
-            }
+            } else
+                break;
         }
-        if(found == -1)
+        if(i == this.getTotalPlayersNumber())
             throw new IllegalStateException("No free bookshelf available");
         else
-            bookshelves[found] = new Bookshelf(player,new PersonalGoalCard(randomNumbers[found]));
+            bookshelves[i] = new Bookshelf(player,new PersonalGoalCard(randomNumbers[i]));
     }
+
     //methods
     public int getGameID(){
         return this.gameID;
@@ -57,9 +59,10 @@ public class Game {
         return this.bag;
     }
 
-    public Bookshelf[] getBookshelf() {
+    public Bookshelf[] getBookshelves() {
         return this.bookshelves;
     }
+
     public int[] extractRandomCardIDs(int amount){
         //extracts indexes for random personal goal cards with no duplicates
         ArrayList<ArrayList<String>> cards = Constants.getPersonalGoalCards();
