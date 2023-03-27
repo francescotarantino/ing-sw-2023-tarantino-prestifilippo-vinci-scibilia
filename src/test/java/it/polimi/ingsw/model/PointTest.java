1cb9924fc4eef26f2d98b19a285fb77d8e1d86ba
Francesco Tarantino <5649257+francescotarantino@users.noreply.github.com>
2023-03-27 09:31:58 +0200

    TileTest equals check PointTest now uses Constants

--- src/test/java/it/polimi/ingsw/model/PointTest.java
+++ src/test/java/it/polimi/ingsw/model/PointTest.java
@@ -1,5 +1,6 @@
 package it.polimi.ingsw.model;
 
+import it.polimi.ingsw.Constants;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -9,11 +10,11 @@ public class PointTest {
     @Test
     void checkPoint(){
         // 9 x 9 max dimension of LivingRoomBoard
-        for(int i = 0; i < 9 ; i++){
-            for( int j = 0; j < 9; j++){
-                Point point = new Point(i,j);
+        for(int i = 0; i < Constants.livingRoomBoardX; i++){
+            for(int j = 0; j < Constants.livingRoomBoardY; j++){
+                Point point = new Point(i, j);
                 assertEquals(point.getX(), i);
-                assertEquals(point.getY(),j);
+                assertEquals(point.getY(), j);
             }
 
         }
--- src/test/java/it/polimi/ingsw/model/TileTest.java
+++ src/test/java/it/polimi/ingsw/model/TileTest.java
@@ -4,6 +4,7 @@ import it.polimi.ingsw.Constants;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class TileTest {
     @Test
@@ -13,5 +14,15 @@ public class TileTest {
             assertEquals(myTile.getType(), Constants.TileType.values()[i]);
             assertEquals(myTile.getVariant(), 1);
         }
+    }
+
+    @Test
+    void checkEquals(){
+        Tile myTile = new Tile(Constants.TileType.values()[0], 1);
+        Tile myTile2 = new Tile(Constants.TileType.values()[0], 1);
+        assertEquals(myTile, myTile2);
+
+        Tile myTile3 = new Tile(Constants.TileType.values()[0], 2);
+        assertNotEquals(myTile, myTile3);
     }
 }
