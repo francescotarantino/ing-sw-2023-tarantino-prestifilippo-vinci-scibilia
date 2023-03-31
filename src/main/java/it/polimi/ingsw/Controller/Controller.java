98d6952fde36af4150836a694d6bb12f41f81efe
Davide Vinci <davide.vinci@mail.polimi.it>
2023-03-31 19:09:25 +0200

    expanded method nextTurn, fixed access to currentPlayer and added private attribute for indexing players

--- src/main/java/it/polimi/ingsw/Controller/Controller.java
+++ src/main/java/it/polimi/ingsw/Controller/Controller.java
@@ -1,15 +1,16 @@
 package it.polimi.ingsw.Controller;
 
-import it.polimi.ingsw.model.Game;
-import it.polimi.ingsw.model.LivingRoomBoard;
-import it.polimi.ingsw.model.Point;
-import it.polimi.ingsw.model.Tile;
-import it.polimi.ingsw.model.exceptions.IllegalPositionException;
+import it.polimi.ingsw.Constants;
+import it.polimi.ingsw.model.*;
 
 public class Controller {
     //private View view;        TODO: to be implemented
 
     private Game game;
+
+    private Player CurrentPlayer;
+
+    private int PlayerIndex;
 
 
     public Controller(Game game /* , View view */){
@@ -20,23 +21,103 @@ public class Controller {
 
     public void start(){
         //TODO
-        game.modifiesCurrentPlayer(0);
+
+
+        //populating the LivingroomBoard
+        for (int i = 0; i < Constants.livingRoomBoardY; i++) {
+            for (int j = 0; j < Constants.livingRoomBoardX; j++) {
+                Tile CurrentTile = game.getLivingRoomBoard().getTile(new Point(j, i));
+                if (CurrentTile == null) {
+                    Tile newtile = game.getBag().getRandomTile();
+                    game.getLivingRoomBoard().insertTile(newtile, new Point(j, i));
+                }
+            }
+        }
+        //selecting first player
+        PlayerIndex =0;
+        CurrentPlayer = game.getBookshelves()[PlayerIndex].getPlayer();
     };
 
-    public void nextTurn(){
+    public void nextTurn() {
 
-        //control if refilling the living room is needed
+        //controlling if a refill of the living room is needed
 
+        // if there are only tiles without any other adjacent tile
 
-        //control if player have achieved the requirements of a common goal card ----> common goal card implementation is needed
+        int numTile = 0;
+        int onlyTile = 0;
 
-        //control if a player has filled their bookshelf
+        for (int i = 0; i < Constants.livingRoomBoardY; i++) {
+            for (int j = 0; j < Constants.livingRoomBoardX; j++) {
+                Tile CurrentTile = game.getLivingRoomBoard().getTile(new Point(j, i));
+                if (CurrentTile.getType() != Constants.TileType.PLACEHOLDER && CurrentTile != null) {
+                    numTile++;
+                    if (j != Constants.livingRoomBoardX) {
+                        boolean b = game.getLivingRoomBoard().getTile(new Point(j + 1, i)).getType() == Constants.TileType.PLACEHOLDER;
+                        boolean c = game.getLivingRoomBoard().getTile(new Point(j + 1, i)) == null;
+                        if (b&&c) {
+                            if (j != 0) {
+                                b = game.getLivingRoomBoard().getTile(new Point(j - 1, i)).getType() == Constants.TileType.PLACEHOLDER;
+                                c = game.getLivingRoomBoard().getTile(new Point(j - 1, i)) == null;
+                                if (b&&c) {
+                                    if (i != Constants.livingRoomBoardY) {
+                                        b = game.getLivingRoomBoard().getTile(new Point(j, i + 1)).getType() == Constants.TileType.PLACEHOLDER;
+                                        c = game.getLivingRoomBoard().getTile(new Point(j , i+1)) == null;
+                                        if (b&&c) {
+                                            if (i != 0) {
+                                                b = game.getLivingRoomBoard().getTile(new Point(j, i - 1)).getType() == Constants.TileType.PLACEHOLDER;
+                                                c = game.getLivingRoomBoard().getTile(new Point(j , i-1)) == null;
+                                                if (b&&c) {
+                                                    onlyTile++;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (onlyTile == numTile) { // proceed to empty the livingroomBoard
+            for (int i = 0; i < Constants.livingRoomBoardY; i++) {
+                for (int j = 0; j < Constants.livingRoomBoardX; j++) {
+                    Tile CurrentTile = game.getLivingRoomBoard().getTile(new Point(j, i));
+                    if (CurrentTile.getType() != Constants.TileType.PLACEHOLDER && CurrentTile != null) {
+                        game.getLivingRoomBoard().removeTile(new Point(j, i));
+                        game.getBag().pushTile(CurrentTile);
+                    }
+                }
+            }
+            //   populating the LivingRoomBoard
+            for (int i = 0; i < Constants.livingRoomBoardY; i++) {
+                for (int j = 0; j < Constants.livingRoomBoardX; j++) {
+                    Tile CurrentTile = game.getLivingRoomBoard().getTile(new Point(j, i));
+                    if (CurrentTile == null) {
+                        Tile newtile = game.getBag().getRandomTile();
+                        game.getLivingRoomBoard().insertTile(newtile, new Point(j, i));
+                    }
+                }
+            }
 
+        }
 
-        if(game.getCurrentPlayer()  < game.getBookshelf().length)
-                game.modifiesCurrentPlayer(game.getCurrentPlayer() + 1 );
-        else game.modifiesCurrentPlayer(0);
-    };
+        //todo:control if player have achieved the requirements of a common goal card ----> common goal card implementation is needed
+
+        //todo:control if a player has filled their bookshelf
+
+        // changing the current player
+        if (CurrentPlayer != game.getBookshelves()[game.getBookshelves().length - 1].getPlayer()) {
+            PlayerIndex++;
+            CurrentPlayer = game.getBookshelves()[PlayerIndex].getPlayer();
+        } else {
+            PlayerIndex = 0;
+            CurrentPlayer = game.getBookshelves()[PlayerIndex].getPlayer();
+        }
+
+    }
+
         //TODO: to be implemented controls on insertion/removing of tiles
     public Tile[] takeTiles(Point...points) throws IllegalPositionException {
         Tile[] tiles = new Tile[points.length];
