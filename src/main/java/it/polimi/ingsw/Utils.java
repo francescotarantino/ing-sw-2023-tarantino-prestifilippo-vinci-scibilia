97ff958c76a76d292e60d52a02d77505726cdde9
Francesco Tarantino <5649257+francescotarantino@users.noreply.github.com>
2023-04-02 19:06:06 +0200

    Bookshelf has a new attribute that saves which CGC is completed Added some documentation

--- src/main/java/it/polimi/ingsw/Utils.java
+++ src/main/java/it/polimi/ingsw/Utils.java
@@ -1,6 +1,11 @@
 package it.polimi.ingsw;
 
 public class Utils {
+    /**
+     * Checks if the matrix has the correct size of the bookshelf (Constants.bookshelfX x Constants.bookshelfY)
+     * @param matrix matrix to check
+     * @param <T> type of the matrix
+     */
     public static <T> void checkMatrixSize(T[][] matrix){
         if(matrix.length != Constants.bookshelfX)
             throw new IllegalArgumentException("Matrix must be " + Constants.bookshelfX + "x" + Constants.bookshelfY);
--- src/main/java/it/polimi/ingsw/model/Bookshelf.java
+++ src/main/java/it/polimi/ingsw/model/Bookshelf.java
@@ -5,25 +5,63 @@ import it.polimi.ingsw.model.goal_cards.PersonalGoalCard;
 
 public class Bookshelf {
     private final Player player;
-
     private final PersonalGoalCard personalGoalCard;
-
     private final Tile[][] matrix;
+    private final boolean[] completedCommonGoalCards;
 
+    /**
+     * Creates a new Bookshelf for the specified player.
+     * @param newPlayer the player who owns the bookshelf
+     * @param personalGoalCard the personal goal card chosen for the player
+     */
     public Bookshelf(Player newPlayer, PersonalGoalCard personalGoalCard) {
         this.player = newPlayer;
         this.personalGoalCard = personalGoalCard;
         this.matrix = new Tile[Constants.bookshelfX][Constants.bookshelfY];
+        this.completedCommonGoalCards = new boolean[Constants.maxCommonGoalCards];
     }
 
+    /**
+     * Returns the player who owns the bookshelf.
+     * @return the player
+     */
     public Player getPlayer() {
         return this.player;
     }
 
+    /**
+     * Returns the personal goal card associated to the bookshelf.
+     * @return the personal goal card
+     */
     public PersonalGoalCard getPersonalGoalCard() {
         return this.personalGoalCard;
     }
 
+    /**
+     * Checks if the common goal card at the specified index is completed.
+     * @param index the index of the common goal card to be checked
+     * @return true if the common goal card at the specified index is completed, false otherwise
+     */
+    public boolean isCommonGoalCardCompleted(int index) {
+        if(index < 0 || index >= Constants.maxCommonGoalCards)
+            throw new IndexOutOfBoundsException("Invalid index");
+
+        return this.completedCommonGoalCards[index];
+    }
+
+    /**
+     * Returns the matrix of the bookshelf.
+     * @return the matrix of tiles
+     */
+    public Tile[][] getMatrix() {
+        return this.matrix;
+    }
+
+    /**
+     * Returns the tile at the specified coordinates.
+     * @param point the coordinates of the tile
+     * @return the tile at the specified coordinates
+     */
     public Tile getTile(Point point) {
         if(point.getX() < 0 || point.getX() >= Constants.bookshelfX || point.getY() < 0 || point.getY() >= Constants.bookshelfY)
             throw new IndexOutOfBoundsException("Invalid bookshelf coordinates");
@@ -31,6 +69,12 @@ public class Bookshelf {
         return this.matrix[point.getX()][point.getY()];
     }
 
+    /**
+     * Inserts the specified tile at the specified coordinates.
+     * Throws exceptions if the coordinates are not valid.
+     * @param point the coordinates where the tile will be inserted
+     * @param tile the tile to be inserted
+     */
     public void insertTile(Point point, Tile tile) {
         if (point == null || tile == null){
             throw new NullPointerException("Arguments cannot be null");
@@ -43,5 +87,20 @@ public class Bookshelf {
         }
 
         this.matrix[point.getX()][point.getY()] = tile;
+    }
+
+    /**
+     * Set the common goal card at the specified index as completed.
+     * If the common goal card is already completed, an exception is thrown.
+     * @param index the index of the common goal card to be set as completed
+     */
+    public void setCompletedCommonGoalCards(int index) {
+        if(index < 0 || index >= Constants.maxCommonGoalCards)
+            throw new IndexOutOfBoundsException("Invalid index");
+
+        if (this.completedCommonGoalCards[index])
+            throw new IllegalArgumentException("The common goal card is already completed");
+
+        this.completedCommonGoalCards[index] = true;
     }
 }
