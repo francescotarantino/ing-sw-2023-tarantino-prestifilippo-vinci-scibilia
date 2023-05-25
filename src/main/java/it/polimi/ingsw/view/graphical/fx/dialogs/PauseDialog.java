285012d1c703b6dee94c48567b0ec32f785a6e34
Francesco Tarantino <5649257+francescotarantino@users.noreply.github.com>
2023-05-25 12:26:07 +0200

    Quit button in pause dialog

--- src/main/java/it/polimi/ingsw/view/graphical/fx/dialogs/PauseDialog.java
+++ src/main/java/it/polimi/ingsw/view/graphical/fx/dialogs/PauseDialog.java
@@ -1,7 +1,12 @@
 package it.polimi.ingsw.view.graphical.fx.dialogs;
 
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 /**
  * This dialog is used to display the pause message when a player is the only one left in the game.
@@ -13,7 +18,20 @@ public class PauseDialog extends Dialog<Void> {
 
         setTitle("Game paused");
         setHeaderText("The game has been paused since you're the only player left in the game.");
-        setContentText("Please wait for other players to reconnect...");
+
+        Label label = new Label("Please wait for other players to reconnect...");
+        Button button = new Button("Quit");
+        button.setOnAction(event -> System.exit(0));
+        button.setAlignment(Pos.CENTER_RIGHT);
+
+        VBox vBox = new VBox();
+        vBox.getChildren().add(label);
+        HBox hBox = new HBox();
+        hBox.getChildren().add(button);
+        hBox.setAlignment(Pos.CENTER_RIGHT);
+        vBox.getChildren().add(hBox);
+
+        getDialogPane().setContent(vBox);
     }
 
     public void showIfNotShown() {
